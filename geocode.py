b2eaf6b3dc39a9de11ce0dce08ea39b40643c787
DennisNenov <nycdennen@gmail.com>
2015-01-15 13:20:59 -0500

    update

--- geocode.py
+++ geocode.py
@@ -1,9 +1,10 @@
 import urllib2
 import json
-
 def return_cor(x):
-  url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + x + "&key=AIzaSyDbtcCT9g5ulN1-ISPD_XyHxp8w_wadh3U"
-  request = urllib2.urlopen(url)
-  result = request.read()
-  d = json.loads(result)
-  return d['geometry']['location']['lat'] + ' , ' + d['geometry']['location']['long'] 
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + x + "&key=AIzaSyDbtcCT9g5ulN1-ISPD_XyHxp8w_wadh3U"
+    request = urllib2.urlopen(url)
+    result = request.read()
+    d = json.loads(result)
+    return d['results']['geometry']['location']['lat'] + ' , ' + d['results']['geometry']['location']['long']
+
+print return_cor("345+chambers+st+NY")
