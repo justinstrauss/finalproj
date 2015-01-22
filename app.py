198c91d5328c403fb135ea5f727169344dfaccab
Lev Akabas <lakabas15@gmail.com>
2015-01-22 12:41:38 -0500

    form and yelp

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 import db
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import urllib2, json, urllib
+import yelp
 
 app = Flask(__name__)
 
@@ -27,10 +28,24 @@ def index():
 def account():
     return render_template("account.html")
 
-@app.route('/create')
+@app.route('/create', methods=['GET','POST'])
 #@login_required
 def create():
-	return render_template("create.html")
+    if request.method=='GET':
+        return render_template("create.html")
+    search = request.form['activityEntry']
+    cll = request.form['locationEntry']
+    print cll
+    session['search'] = search
+    session['cll'] = cll
+    if (cll == None or search == None):
+        return render_template("index.html")
+    else:
+        return redirect(url_for('results'))
+
+@app.route('/results')
+def results():
+    return yelp.search(session.pop('search',None),session.pop('cll',None))
 
 if __name__ == '__main__':
         db.setup()
--- yelp.py
+++ yelp.py
@@ -7,13 +7,13 @@ import json
 import urllib
 import urllib2
 
-def search(keyword, city):
+def search(keyword, cll):
     consumer_key = 'InepiTUxl4_CN8hRCxV4gA'
     consumer_secret = 'ZzuBLqtEreWlm1FqBltPEImuZ2Q'
     token = '7uAeNaimqgTsYqCzboSZylZUukFJ1-I-'
     token_secret = '22FbVEI8cfO63ROlQDV_bePx3XU'
     consumer= oauth2.Consumer(consumer_key,consumer_secret)
-    url= 'http://api.yelp.com/v2/search?term=%s&location=%s&limit=%d&format=json'%(keyword, city, 5)
+    url= 'http://api.yelp.com/v2/search?term=%s&cll=%s&limit=%d&format=json'%(keyword, cll, 5)
     oauth_request = oauth2.Request('GET', url, {})
     oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
                           'oauth_timestamp': oauth2.generate_timestamp(),
@@ -26,10 +26,19 @@ def search(keyword, city):
         conn = urllib2.urlopen(signed_url)
         try:
             results =json.load(conn)
-            print response
+            print results + "hi"
+            return results
         finally:
+            print "hello"
             conn.close()
+    finally:
+        return ""
 
-        
 if __name__ == "__main__":
-    search('tacos','San+Francisco')
+    search("tacos","40.9,-74")
+
+
+
+            
+            
+            
